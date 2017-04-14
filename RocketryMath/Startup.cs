a20cfac8a715488974e66eb879592e40080a7d77
Chris Wilson <chris@ocmit.com>
2017-04-14 13:48:44 -0700

    Add project files.

---
+++ RocketryMath/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RocketryMath.Startup))]
+namespace RocketryMath
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
